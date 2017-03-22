3971513dc02ac55426a5ac641bff05db4d9f19e7
Andrey Ershov <ershov.andrey@gmail.com>
2017-03-22 23:21:38 +0200

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install command-line tools using Homebrew
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+# GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+brew install moreutils
+# GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
+brew install findutils
+
+brew install wget --with-iri
+
+# Install more recent versions of some OS X tools
+brew install vim --with-override-system-vi
+brew install homebrew/dupes/nano
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
+brew install homebrew/dupes/screen
+
+# mtr - ping & traceroute. best.
+brew install mtr
+
+    # allow mtr to run without sudo
+    mtrlocation=$(brew info mtr | grep Cellar | sed -e 's/ (.*//') #  e.g. `/Users/paulirish/.homebrew/Cellar/mtr/0.86`
+    sudo chmod 4755 $mtrlocation/sbin/mtr
+    sudo chown root $mtrlocation/sbin/mtr
+
+brew install tree
+
+brew install htop
+
+brew install node # This installs `npm` too using the recommended installation method
+
+brew install caskroom/cask/brew-cask
+
+# Remove outdated versions from the cellar
+brew cleanup
+
+# Type `git open` to open the GitHub page or website for a repository.
+npm install -g git-open
+
+# fancy listing of recent branches
+npm install -g git-recent
+
+# sexy git diffs
+npm install -g diff-so-fancy
+
+# trash as the safe `rm` alternative
+npm install --global trash-cli
+
+#oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+
+sh .osx
